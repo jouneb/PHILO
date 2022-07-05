640ae6288be22ffd69fb42695f25c82cb4caa10a
jacinthe <jbouyer@student.42.fr>
2022-07-05 13:11:38 +0200

    test

---
+++ exploration.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbouyer <jbouyer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/30 11:48:48 by jbouyer           #+#    #+#             */
+/*   Updated: 2022/07/01 11:00:48 by jbouyer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//https://sites.uclouvain.be/SystInfo/notes/Theorie/Threads/coordination.html
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+pthread_mutex_t wait_me;
+
+void   *test(void *lol)
+{
+    pthread_mutex_lock(&wait_me);
+    if (lol == "a")
+        printf("Kant se promene\n");
+    if (lol == "b")
+        printf("Kirkegaard pense\n");
+    if (lol == "c")
+        printf("Nietzsche dort\n");
+    pthread_mutex_unlock(&wait_me);
+    return (NULL);
+}
+
+int main()
+{
+    pthread_t   Kant;
+    pthread_t   Kierkegaard;
+    pthread_t   Nietzsche;
+    char *str;
+    char *lol;
+    char *tmp;
+
+    str = "b";
+    lol = "a";
+    tmp = "c";
+    pthread_mutex_init(&wait_me, NULL);
+    pthread_create(&Kant, NULL, test, lol);
+    pthread_create(&Kierkegaard, NULL, test, str);
+    pthread_create(&Nietzsche, NULL, test, tmp);
+    pthread_join(Kant, NULL);
+    pthread_join(Kierkegaard, NULL);
+    pthread_join(Nietzsche, NULL);
+    pthread_mutex_destroy(&wait_me);
+    return (0);
+
+}
+
---
+++ philo.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbouyer <jbouyer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/30 11:48:53 by jbouyer           #+#    #+#             */
+/*   Updated: 2022/07/04 16:37:18 by jbouyer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H  
+
+typedef struct s_philosopher
+{
+    //le num d'ID;
+    //le num de la fourhcettedroite
+    //le num de la fourchette gauche
+    // le nombre de fois ou il a bouffe
+    //?
+    //la struct globale?
+}   t_philospher;
+
+typedef struct s_global
+{
+    //nombre de fois ou ils doivent boufffer si jamasi il y a ? pas sur que ce soit pertinent ici.
+    //mutex sur les fourchettes;
+
+}   t_global;
+
+#endif
